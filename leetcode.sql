cab47bbaff4c38cdf4278dd618723aa6c6b918e0
SyroidNadiia <shcherbelova@ukr.net>
2025-08-05 13:29:53 +0300

    solved a few exercises

--- leetcode.sql
+++ leetcode.sql
@@ -468,3 +468,39 @@ WITH cte AS (
 SELECT visited_on, amount, average_amount
 FROM cte
 WHERE rn >= 7;
+
+
+--1757. Recyclable and Low Fat Products
+select product_id
+from Products
+where low_fats = 'Y' and recyclable = 'Y';
+
+--584. Find Customer Referee
+select name
+from Customer
+where referee_id != 2 or referee_id is null
+
+--595. Big Countries
+select name, population, area
+from World
+where area >= 3000000 or population >= 25000000
+
+--1148. Article Views I
+select author_id as id
+from Views
+where author_id = viewer_id
+group by id 
+order by id ASC
+
+--1683. Invalid Tweets
+select tweet_id
+from Tweets
+where length(content) > 15
+
+
+--1378. Replace Employee ID With The Unique Identifier
+select DISTINCT(e.name), eu.unique_id
+from Employees as e
+left join EmployeeUNI as eu
+on e.id = eu.id
+
